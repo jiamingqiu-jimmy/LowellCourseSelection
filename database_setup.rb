74dcf9f83ae637ffab032c6d641b290677a0145b
Jimmy <jiamingqiu951@gmail.com>
2015-08-08 07:53:57 +0000

    Tried a new way to group Database setup

--- database_setup.rb
+++ database_setup.rb
@@ -1,7 +1,16 @@
-if ENV['RACK_ENV'] == 'production'
-  # We're running on Heroku
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-else
-  # We're running locally
-  DataMapper.setup(:default, "sqlite:online-arena.db")
+# if ENV['RACK_ENV'] == 'production'
+#   # We're running on Heroku
+#   DataMapper.setup(:default, ENV['DATABASE_URL'])
+# else
+#   # We're running locally
+#   DataMapper.setup(:default, "sqlite:online-arena.db")
+# end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/online-arena.db")
 end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
