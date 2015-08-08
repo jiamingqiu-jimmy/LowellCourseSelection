3b50daa8a37b298147684dbb15ec8a6b1ca2684b
Jimmy <jiamingqiu951@gmail.com>
2015-08-08 08:30:15 +0000

    Changed database_setup back to original

--- database_setup.rb
+++ database_setup.rb
@@ -1,18 +1,18 @@
 require 'data_mapper'
 
-# if ENV['RACK_ENV'] == 'production'
-#   # We're running on Heroku
-#   DataMapper.setup(:default, ENV['DATABASE_URL'])
-# else
-#   # We're running locally
-#   DataMapper.setup(:default, "sqlite:online-arena.db")
+if ENV['RACK_ENV'] == 'production'
+  # We're running on Heroku
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+else
+  # We're running locally
+  DataMapper.setup(:default, "sqlite:online-arena.db")
+end
+
+# configure :development do
+#   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/online-arena.db")
 # end
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/online-arena.db")
-end
-
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
+# configure :production do
+#   DataMapper.setup(:default, ENV['DATABASE_URL'])
+# end
 
