13ec7464cbf0348a82c25710b7013b12c101a289
Jimmy <jiamingqiu951@gmail.com>
2015-08-08 20:10:48 +0000

    Added default user admin

--- seed.rb
+++ seed.rb
@@ -13,3 +13,19 @@ DEFAULT_REGISTRIES.each do |registry_data|
   puts "Creating #{registry_data[:name]}..."
   Registry.create(registry_data)
 end
+
+DEFAULT_USERS = [
+  {
+    username: "Admin",
+    email: "admin@gmail.com",
+    password: "123456",
+    password_confirmation: "123456",
+    admin: true,
+    registry_id: "1"
+  }
+]
+
+DEFAULT_USERS.each do |user_data|
+  puts "Creating #{user_data[:name]}..."
+  User.create(user_data)
+end
